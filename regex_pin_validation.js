f67eee374169a7ab7bafe095b9c343e991bfedea
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-06 01:07:20 +0530

    Created regex pattern to validated pincode

--- regex_pin_validation.js
+++ regex_pin_validation.js
@@ -1,1 +1,21 @@
-console.log("Welcome to the regex pattern");
+//uc1 pincode validation for  6 digit number
+let PincodeRegex = RegExp("[1-9]{1}[0-9]{5}");
+function PincodeValidation(pincode)
+{
+    if (PincodeRegex.test(pincode) && pincode.length === 6) 
+    {
+        console.log(pincode + "  is in valid format");
+    }
+    else throw "Invalid Pincode Format";
+}
+try 
+{
+    //readline-sync waits for users response and make it synchronised
+    var readline = require('readline-sync');
+    let pincode = readline.question(" Please Enter the pincode for validation: ");
+    PincodeValidation(pincode);
+}
+catch (e)
+{
+    console.log(e);
+}
