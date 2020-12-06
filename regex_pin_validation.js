a352cb0092a17b82d7cfa8d603f3c552124e904e
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-06 14:24:39 +0530

    Added spacing after 3 digit validation

--- regex_pin_validation.js
+++ regex_pin_validation.js
@@ -1,20 +1,24 @@
 //uc1 pincode validation for  6 digit number
 // the following pattern validates numbers only as input , 
-//pincodeRegex restricts special characters or alphabets (uc2) at start
- //pincodeRegex restricts special characters or alphabets (uc3) at end (uc3)
-let pincodeRegex = RegExp("[1-9]{1}[0-9]{5}");
-function PincodeValidation(pincode) {
-    if (pincodeRegex.test(pincode) && pincode.length === 6) {
+//pincodeRegex restricts special characters or alphabets (uc2) at start as well as at end (uc3)
+// uc4 spacing after 3 digit is added
+let pincodeRegex = RegExp("^[0-9]{3}[ ]*[0-9]{3}$");
+function PincodeValidation(pincode) 
+{
+    if (pincodeRegex.test(pincode)) 
+    {
         console.log(pincode + "  is in valid format");
     }
     else throw "Invalid Pincode Format";
 }
-try {
+try 
+{
     //readline-sync waits for users response and make it synchronised
     var readline = require('readline-sync');
     let pincode = readline.question(" Please Enter the pincode for validation: ");
     PincodeValidation(pincode);
 }
-catch (e) {
+catch (e)
+{
     console.log(e);
 }
