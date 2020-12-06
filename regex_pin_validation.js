5706cbe802cf4db66ad8b902dddd671300d9bc4f
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-06 14:18:26 +0530

    Restrict the pincode from taking alphabets or special characters at the end

--- regex_pin_validation.js
+++ regex_pin_validation.js
@@ -1,22 +1,20 @@
 //uc1 pincode validation for  6 digit number
-//uc2 pattern validates numbers only as input , 
-//it restricts special characters or alphabets
+// the following pattern validates numbers only as input , 
+//pincodeRegex restricts special characters or alphabets (uc2) at start
+ //pincodeRegex restricts special characters or alphabets (uc3) at end (uc3)
 let pincodeRegex = RegExp("[1-9]{1}[0-9]{5}");
-function PincodeValidation(pincode) 
-{
+function PincodeValidation(pincode) {
     if (pincodeRegex.test(pincode) && pincode.length === 6) {
         console.log(pincode + "  is in valid format");
     }
     else throw "Invalid Pincode Format";
 }
-try 
-{
+try {
     //readline-sync waits for users response and make it synchronised
     var readline = require('readline-sync');
     let pincode = readline.question(" Please Enter the pincode for validation: ");
     PincodeValidation(pincode);
 }
-catch (e)
- {
+catch (e) {
     console.log(e);
 }
